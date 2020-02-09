7b836de4ce116b2a9b97b5979a4978aca1caca36
pechischev <pechischev.vladimir@gmail.com>
2020-02-09 22:29:20 +0300

    Updates

---
+++ precache-manifest.461636287cc03c6ad31654f7d1536118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a400820e4ad97a2b6168df44e8a43613",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43ebc898f4dd96c04f9b",
+    "url": "/static/css/main.d8872425.chunk.css"
+  },
+  {
+    "revision": "917f724e043784472169",
+    "url": "/static/js/2.4ae57364.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4ae57364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ebc898f4dd96c04f9b",
+    "url": "/static/js/main.9341ae00.chunk.js"
+  },
+  {
+    "revision": "1856d081ba0d38757ee7",
+    "url": "/static/js/runtime-main.af4025e2.js"
+  }
+]);
